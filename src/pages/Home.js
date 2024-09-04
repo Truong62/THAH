71f0150460c6258703579c0d6b5118caec8fdd3b
Truong62 <truong06022004@gmial.com>
2024-09-04 10:49:43 +0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,13 +6,15 @@ const Home = () => {
     <React.Fragment>
       <Layout>
         <div className="flex justify-around items-center my-6">
-          <div>
+          <div className="flex items-center">
             <img
-              className="w-[80px] rounded-2xl"
+              className="w-[50px] rounded-2xl"
               src="https://yt3.googleusercontent.com/HnAnUZS7d8LFtPHaHRtPH9nMoUmrsBBy_FDD-T-YQIycPW55peuWo1NO_rxPIswDbukXYlk7=s160-c-k-c0x00ffffff-no-rj"
               alt=""
             />
-            <input></input>
+            <div className="flex items-center ml-3 shadow-lg w-[250px] rounded-full p-4">
+              <input className="h-full outline-none" placeholder="Sreach ...."></input>
+            </div>
           </div>
           <div>dsdasdsa</div>
         </div>
