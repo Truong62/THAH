be5cf01daa2dd60ebd1013e67789467581327f1b
Hyp2k4 <hiephoang1752004@gmail.com>
2024-09-05 23:41:13 +0700

    update square pic

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,7 +9,7 @@ const CardProduct = ({ link, nameProduct, description, price, brand, nametag }) 
             <Link to={`/campaign/${link}`}
                 className="bg-white rounded-lg p-2  transition-transform duration-300 ease-in-out flex flex-col justify-between h-full hover:translate-y-[-5px]">
                 <img src='../../images/wallpaperflare.com_wallpaper.jpg' alt="Campaign"
-                    className="w-full h-auto max-h-52 rounded-lg object-cover" />
+                    className="w-full h-auto max-h-52 object-cover" style={{ width: '100%', height: '100%', aspectRatio: '1 / 1' }} />
                 <div className="flex flex-col gap-2.5 mt-2.5 flex-grow">
                     <h4 className="text-2xl font-bold ">{nameProduct}</h4>
                     <p>{description}</p>
