3a3ab63dbb376605ecb75c6dc5192a5fce469487
Truong62 <truong06022004@gmial.com>
2024-09-09 22:26:48 +0700

    update

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,9 +4,8 @@ import {Link} from 'react-router-dom';
 const CardProduct = ({link, nameProduct, description, price, brand, nameTag = []}) => {
     return (
         <Link to={`/campaign/${link}`}>
-            <div
-                className="bg-white rounded-lg p-4 shadow-lg transition-transform duration-300 ease-in-out flex flex-col justify-between h-full hover:translate-y-[-5px]">
-                <img src='../../images/AIR+ZOOM+PEGASUS+41+PQ.png' alt="Campaign" className="w-full h-auto object-cover"
+            <div className="bg-white rounded-lg p-3 border-gray-200 border transition-transform duration-300 ease-in-out flex flex-col justify-between h-full hover:translate-y-[-5px]">
+                <img src='https://bizweb.dktcdn.net/thumb/1024x1024/100/413/335/products/duyetfashion-wmns-nike-air-jordan-1-retro-high-og-silver-toe-cd0461-001-duyet-fashion-51.jpg?v=1627397656580' alt="Campaign" className="w-full rounded-t-lg h-auto object-cover"
                      style={{aspectRatio: '1 / 1'}}/>
                 <span className=" font-semibold text-gray-500 mt-2">{brand}</span>
                 <h4 className="text-xl font-bold mb-2">{nameProduct}</h4>
