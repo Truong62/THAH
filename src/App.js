da5be83f58c11c2436e96a25189cacd73f6faa6a
Truong62 <truong06022004@gmial.com>
2024-09-11 21:35:25 +0700

    dasd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import AppRoutes from "./routes/routes";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BreadCrumb from "./components/BreadCrumb";
+
 function App() {
   return (
     <div>
-      <BreadCrumb/>
       <AppRoutes></AppRoutes>
     </div>
   );
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import Home from '../pages/Home';
 import Blogs from "../pages/Blogs";
+import BreadCrumb from "../components/BreadCrumb";
 
 const AppRoutes = () => {
     return (
@@ -9,7 +10,7 @@ const AppRoutes = () => {
             <Route path="/" element={<Home></Home>}/>
             <Route path="/blogs" element={<Blogs></Blogs>}/>
             {/* test */}
-            <Route path="/huong" element={<h1>huong</h1>}/>
+            <Route path="/blogs/huong" element={  <BreadCrumb/>}/>
             <Route path="/hiep" element={<h1>hiep</h1>}/>
         </Routes>
     );
