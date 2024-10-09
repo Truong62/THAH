85db52dac44434b4f03bac7510cb83182575eadb
HuyHuongg <vuhuyhuong03112004@gmail.com>
2024-10-09 22:43:30 +0700

    fix suggest

--- src/components/ProductDetailsCard.js
+++ src/components/ProductDetailsCard.js
@@ -1,15 +1,51 @@
-import React, {useState, memo} from "react";
-import {useParams} from "react-router-dom";
+import React, { useState, memo } from "react";
+import { useParams } from "react-router-dom";
 import products from "./ProductTest"; // Import the products array
-import {formatCurrency} from "../utils/formatCurrency"; // Import formatCurrency
+import { formatCurrency } from "../utils/formatCurrency"; // Import formatCurrency
+import SuggestProducts from "../components/suggestProducts";
 
 const ProductDetailsCard = () => {
-    const {link} = useParams();
+    const { link } = useParams();
     const product = products.find((p) => p.link === link);
 
     const [mainImageIndex, setMainImageIndex] = useState(0);
     const [selectedColor, setSelectedColor] = useState("black");
     const [selectedSize, setSelectedSize] = useState("M");
+
+    const productData = [
+        {
+            link: "nike-air-jordan-1",
+            name: "Nike Air Jordan 1",
+            description: "Mẫu giày thể thao nổi bật của Nike.",
+            price: 10000000,
+            brand: "Nike",
+            tag: ["Hot", "Sale"]
+        },
+        {
+            link: "nike-air-max",
+            name: "Nike Air Max",
+            description: "Giày thể thao Nike Air Max mới nhất.",
+            price: 12000000,
+            brand: "Nike",
+            tag: ["New"]
+        },
+        {
+            link: "adidas-ultra-boost",
+            name: "Adidas Ultra Boost",
+            description: "Giày chạy bộ Adidas Ultra Boost.",
+            price: 15000000,
+            brand: "Adidas",
+            tag: ["Popular"]
+        },
+        {
+            link: "puma-rs-x",
+            name: "Puma RS-X",
+            description: "Giày thể thao Puma RS-X thời thượng.",
+            price: 11000000,
+            brand: "Puma",
+            tag: ["Trending"]
+        },
+    ];
 
     if (!product) {
         return <div>Product not found</div>; // tạo một trang 404 ở route nếu không có thì đi tới trang đó
@@ -28,124 +64,126 @@ const ProductDetailsCard = () => {
     };
 
     return (
-        <div className="flex flex-col md:flex-row rounded-lg bg-white p-6 max-w-6xl mx-auto">
-            <div className="relative w-full md:w-2/3 mb-4 md:mb-0 md:mr-6">
-                <div className="w-full aspect-w-1 aspect-h-1">
-                    <img
-                        src={product.images && product.images[mainImageIndex]}
-                        alt="Product"
-                        className="w-full h-full object-cover transition duration-300 ease-in-out"
-                    />
-                </div>
-                <button
-                    className="absolute top-1/2 left-0 transform -translate-y-1/2 rounded-full p-2 hover:shadow-md"
-                    onClick={handlePrevImage}
-                >
-                    <svg
-                        className="w-6 h-6"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M15 19l-7-7 7-7"
-                        ></path>
-                    </svg>
-                </button>
-                <button
-                    className="absolute top-1/2 right-0 transform -translate-y-1/2 rounded-full p-2 hover:shadow-md"
-                    onClick={handleNextImage}
-                >
-                    <svg
-                        className="w-6 h-6"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M9 5l7 7-7 7"
-                        ></path>
-                    </svg>
-                </button>
-                {product.images && (
-                    <div className="flex mt-4 gap-2">
-                        {product.images.map((image, index) => (
-                            <div key={index}
-                                 className={`w-[92px] h-[92px] rounded-lg cursor-pointer border overflow-hidden transition duration-300 ease-in-out transform ${
-                                     mainImageIndex === index ? "border-2 border-black scale-105" : ""
-                                 }`}
-                                 onClick={() => setMainImageIndex(index)}>
-                                <img src={image} alt={`Product ${index}`}
-                                     className="w-[92px] h-[92px] object-cover transition duration-300 ease-in-out transform hover:scale-105"/>
-                            </div>
-                        ))}
+        <>
+            <div className="flex flex-col md:flex-row rounded-lg bg-white p-6 max-w-6xl mx-auto">
+                <div className="relative w-full md:w-2/3 mb-4 md:mb-0 md:mr-6">
+                    <div className="w-full aspect-w-1 aspect-h-1">
+                        <img
+                            src={product.images && product.images[mainImageIndex]}
+                            alt="Product"
+                            className="w-full h-full object-cover transition duration-300 ease-in-out"
+                        />
                     </div>
-                )}
-            </div>
-            <div className="w-full md:w-1/3">
-                <div className="text-4xl font-bold mb-2">{product.nameProduct}</div>
-                <div className="text-red-500 text-xl font-bold">
-                    {formatCurrency(product.price)}
-                </div>
-                <div className="mb-2">
-                    <span className="font-bold text-2xl">Color:</span>
-                    {product.colors && (
-                        <div className="flex gap-2 mt-1">
-                            {product.colors.map((color) => (
-                                <div
-                                    key={color}
-                                    className={`flex items-center justify-center w-24 h-10 rounded-full cursor-pointer border ${
-                                        selectedColor === color ? "ring-2 ring-black" : ""
-                                    }`}
-                                    onClick={() => setSelectedColor(color)}
-                                >
-                                    <span className="text-sm">{color}</span>
+                    <button
+                        className="absolute top-1/2 left-0 transform -translate-y-1/2 rounded-full p-2 hover:shadow-md"
+                        onClick={handlePrevImage}
+                    >
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M15 19l-7-7 7-7"
+                            ></path>
+                        </svg>
+                    </button>
+                    <button
+                        className="absolute top-1/2 right-0 transform -translate-y-1/2 rounded-full p-2 hover:shadow-md"
+                        onClick={handleNextImage}
+                    >
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M9 5l7 7-7 7"
+                            ></path>
+                        </svg>
+                    </button>
+                    {product.images && (
+                        <div className="flex mt-4 gap-2">
+                            {product.images.map((image, index) => (
+                                <div key={index}
+                                    className={`w-[92px] h-[92px] rounded-lg cursor-pointer border overflow-hidden transition duration-300 ease-in-out transform ${mainImageIndex === index ? "border-2 border-black scale-105" : ""}`}
+                                    onClick={() => setMainImageIndex(index)}>
+                                    <img src={image} alt={`Product ${index}`}
+                                        className="w-[92px] h-[92px] object-cover transition duration-300 ease-in-out transform hover:scale-105" />
                                 </div>
                             ))}
                         </div>
                     )}
                 </div>
-                <div className="mb-2">
-                    <span className="font-bold text-2xl">Size:</span>
-                    {product.sizes && (
-                        <div className="flex flex-wrap gap-2 mt-1">
-                            {product.sizes.map((size) => (
-                                <div
-                                    key={size}
-                                    className={`px-3 py-1 border rounded-full cursor-pointer text-xl ${
-                                        selectedSize === size ? "bg-gray-300" : ""
-                                    }`}
-                                    onClick={() => setSelectedSize(size)}
-                                >
-                                    {size}
-                                </div>
-                            ))}
-                        </div>
-                    )}
-                </div>
-                <div className="flex flex-col gap-2 mb-2">
-                    <button
-                        className="px-4 py-2 bg-black text-white rounded-full transition duration-300 ease-in-out transform hover:scale-105 text-xl">
-                        Buy Now
-                    </button>
-                    <button
-                        className="px-4 py-2 bg-white border border-black rounded-full transition duration-300 ease-in-out transform hover:scale-105 text-xl">
-                        Add To Cart
-                    </button>
-                </div>
-                <div className="mt-4">
-                    <p>{product.description}</p>
+                <div className="w-full md:w-1/3">
+                    <div className="text-4xl font-bold mb-2">{product.nameProduct}</div>
+                    <div className="text-red-500 text-xl font-bold">
+                        {formatCurrency(product.price)}
+                    </div>
+                    <div className="mb-2">
+                        <span className="font-bold text-2xl">Color:</span>
+                        {product.colors && (
+                            <div className="flex gap-2 mt-1">
+                                {product.colors.map((color) => (
+                                    <div
+                                        key={color}
+                                        className={`flex items-center justify-center w-24 h-10 rounded-full cursor-pointer border ${selectedColor === color ? "ring-2 ring-black" : ""}`}
+                                        onClick={() => setSelectedColor(color)}
+                                    >
+                                        <span className="text-sm">{color}</span>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                    <div className="mb-2">
+                        <span className="font-bold text-2xl">Size:</span>
+                        {product.sizes && (
+                            <div className="flex flex-wrap gap-2 mt-1">
+                                {product.sizes.map((size) => (
+                                    <div
+                                        key={size}
+                                        className={`px-3 py-1 border rounded-full cursor-pointer text-xl ${selectedSize === size ? "bg-gray-300" : ""}`}
+                                        onClick={() => setSelectedSize(size)}
+                                    >
+                                        {size}
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                    <div className="flex flex-col gap-2 mb-2">
+                        <button
+                            className="px-4 py-2 bg-black text-white rounded-full transition duration-300 ease-in-out transform hover:scale-105 text-xl">
+                            Buy Now
+                        </button>
+                        <button
+                            className="px-4 py-2 bg-white border border-black rounded-full transition duration-300 ease-in-out transform hover:scale-105 text-xl">
+                            Add To Cart
+                        </button>
+                    </div>
+                    <div className="mt-4">
+                        <p>{product.description}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className="mt-8 w-full">
+                <h2 className="text-2xl font-bold mb-4">Sản phẩm gợi ý</h2>
+                <div className="bg-gray-100 p-4 rounded-lg">
+                    <SuggestProducts products={productData} />
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -2,49 +2,13 @@ import React from 'react';
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Layout from "../components/Layout";
-import SuggestProducts from "../components/suggestProducts";
 
 
 const Blogs = () => {
-    const productData = [
-        {
-            link: "nike-air-jordan-1",
-            name: "Nike Air Jordan 1",
-            description: "Mẫu giày thể thao nổi bật của Nike.",
-            price: 10000000,
-            brand: "Nike",
-            tag: ["Hot", "Sale"]
-        },
-        {
-            link: "nike-air-max",
-            name: "Nike Air Max",
-            description: "Giày thể thao Nike Air Max mới nhất.",
-            price: 12000000,
-            brand: "Nike",
-            tag: ["New"]
-        },
-        {
-            link: "adidas-ultra-boost",
-            name: "Adidas Ultra Boost",
-            description: "Giày chạy bộ Adidas Ultra Boost.",
-            price: 15000000,
-            brand: "Adidas",
-            tag: ["Popular"]
-        },
-        {
-            link: "puma-rs-x",
-            name: "Puma RS-X",
-            description: "Giày thể thao Puma RS-X thời thượng.",
-            price: 11000000,
-            brand: "Puma",
-            tag: ["Trending"]
-        },
-    ];
     return (
         <>
             <Header />
             <Layout>
-            <SuggestProducts products={productData} />
             </Layout>
             <Footer />
         </>
