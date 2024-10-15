6d100535d4ce5dadc5728a033d28239ec8048673
Hyp2k4 <hiephoang1752004@gmail.com>
2024-10-15 12:37:14 +0700

    update effect

--- src/components/ProductDetailsCard.js
+++ src/components/ProductDetailsCard.js
@@ -1,4 +1,4 @@
-import React, { useState, memo } from "react";
+import React, { useState, useEffect, memo } from "react";
 import { useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
@@ -8,18 +8,26 @@ import products from "./ProductTest"; // Import the products array
 import { formatCurrency } from "../utils/formatCurrency"; // Import formatCurrency
 import { addToCart, updateQuantity } from "../redux/cart/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
-import Alert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar"; // Import Snackbar
+import Slide from "@mui/material/Slide"; // Import Slide for transition
+import Collapse from "@mui/material/Collapse"; // Import Collapse for description
+import Button from "@mui/material/Button"; // Import Button for toggling
 import useDeviceType from "../hooks/useDeviceType"; // Import the device type hook
-import Modal from "@mui/material/Modal"; // Import Modal from Material UI
-import { truncateDescriptionDetails } from "../utils/truncateDescription";
+import WarningAmberIcon from "@mui/icons-material/WarningAmber"; // Import WarningAmberIcon
+
+const SlideTransition = (props) => {
+  return <Slide {...props} direction="left" />;
+};
+
 const ProductDetailsCard = () => {
   const { link } = useParams();
   const product = products.find((p) => p.productName === link);
 
   const dispatch = useDispatch();
   const cartItems = useSelector((state) => state.cart);
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
+  const [snackbarOpen, setSnackbarOpen] = useState(false); // State to control Snackbar visibility
+  const [snackbarMessage, setSnackbarMessage] = useState(""); // State to store Snackbar message
+  const [snackbarType, setSnackbarType] = useState(""); // State to store Snackbar type
 
   const [selectedColor, setSelectedColor] = useState(
     product.variants[0].colorName
@@ -34,7 +42,7 @@ const ProductDetailsCard = () => {
 
   const { isMobile } = useDeviceType(); // Determine if the device is mobile
 
-  const [showMore, setShowMore] = useState(false); // State to control modal visibility
+  const [showFullDescription, setShowFullDescription] = useState(false); // State to control description visibility
 
   if (!product) {
     window.location.href = "/notfound";
@@ -54,8 +62,9 @@ const ProductDetailsCard = () => {
 
   const handleAddToCart = () => {
     if (!selectedColor || !selectedSize) {
-      setError("REQUIRED TO CHOOSE COLOR AND SIZE");
-      setSuccess("");
+      setSnackbarMessage("REQUIRED TO CHOOSE COLOR AND SIZE");
+      setSnackbarType("error");
+      setSnackbarOpen(true);
       return;
     }
 
@@ -73,8 +82,9 @@ const ProductDetailsCard = () => {
           (size) => size.sizeValue === selectedSize
         ).quantity
       ) {
-        setError("OUT OF STOCK");
-        setSuccess("");
+        setSnackbarMessage("OUT OF STOCK");
+        setSnackbarType("error");
+        setSnackbarOpen(true);
         return;
       }
 
@@ -86,7 +96,8 @@ const ProductDetailsCard = () => {
           quantity: existingItem.quantity + 1,
         })
       );
-      setSuccess("QUANTITY UPDATED");
+      setSnackbarMessage("QUANTITY UPDATED");
+      setSnackbarType("success");
     } else {
       const cartItem = {
         id: product.brandId,
@@ -102,14 +113,25 @@ const ProductDetailsCard = () => {
       };
 
       dispatch(addToCart(cartItem));
-      setSuccess("ADDED TO CART");
+      setSnackbarMessage("ADDED TO CART");
+      setSnackbarType("success");
     }
 
-    setError("");
-    setTimeout(() => {
-      setSuccess("");
-    }, 2000);
+    setSnackbarOpen(true);
   };
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
+  useEffect(() => {
+    if (snackbarOpen) {
+      const timer = setTimeout(() => {
+        setSnackbarOpen(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [snackbarOpen]);
 
   return (
     <div className="flex flex-col md:flex-row rounded-lg bg-white p-6 max-w-6xl mx-auto">
@@ -202,6 +224,12 @@ const ProductDetailsCard = () => {
       </div>
       <div className="w-full md:w-1/3">
         <div className="text-4xl font-bold mb-2">{product.productName}</div>
+        <span className="text-black font-bold">
+          Stock Available:{" "}
+          {currentVariant.productColorSize.find(
+            (size) => size.sizeValue === selectedSize
+          )?.quantity || 0}
+        </span>
         <div className="text-red-500 text-xl font-bold">
           {formatCurrency(currentVariant.price)}
         </div>
@@ -253,14 +281,6 @@ const ProductDetailsCard = () => {
             </div>
           )}
         </div>
-        <span className="text-black font-bold">
-          Stock Available:{" "}
-          {currentVariant.productColorSize.find(
-            (size) => size.sizeValue === selectedSize
-          )?.quantity || 0}
-        </span>
-        {error && <Alert severity="error">{error}</Alert>}
-        {success && <Alert severity="success">{success}</Alert>}
         <br></br>
 
         <div className="flex flex-col gap-2 mb-2">
@@ -275,39 +295,46 @@ const ProductDetailsCard = () => {
           </button>
         </div>
         <div className="mt-4">
-          <p>{truncateDescriptionDetails(product.productDescription, 50)}</p>
-          {product.productDescription.split(" ").length > 50 && (
-            <button
-              className="hover:scale-105 text-blue-500"
-              onClick={() => setShowMore(true)}
+          <div className="text-xl font-bold">Description</div>
+          <Collapse in={showFullDescription} collapsedSize={100}>
+            <div
+              style={{
+                maxHeight: showFullDescription ? "300px" : "100px",
+                overflowY: showFullDescription ? "auto" : "hidden",
+                transition: "max-height 0.3s ease",
+              }}
             >
-              ...more
-            </button>
-          )}
-          <Modal
-            open={showMore}
-            onClose={() => setShowMore(false)}
-            aria-labelledby="product-description-modal"
-            aria-describedby="product-description-full"
-          >
-            <div className="bg-white p-4 max-w-lg mx-auto mt-20 rounded-lg shadow-lg">
-              <h2
-                id="product-description-modal"
-                className="text-xl font-bold mb-2"
-              >
-                Description
-              </h2>
-              <p id="product-description-full">{product.productDescription}</p>
-              <button
-                className="mt-4 px-4 py-2 bg-black text-white rounded-full"
-                onClick={() => setShowMore(false)}
-              >
-                Close
-              </button>
+              <p>{product.productDescription}</p>
             </div>
-          </Modal>
+          </Collapse>
+          <Button
+            onClick={() => setShowFullDescription(!showFullDescription)}
+            color="primary"
+          >
+            {showFullDescription ? "Show Less" : "Read More"}
+          </Button>
         </div>
       </div>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+        TransitionComponent={SlideTransition} // Use Slide transition
+        message={
+          <span style={{ display: "flex", alignItems: "center" }}>
+            {snackbarType === "error" && (
+              <WarningAmberIcon style={{ marginRight: 8 }} />
+            )}
+            {snackbarMessage}
+          </span>
+        }
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        ContentProps={{
+          style: {
+            backgroundColor: snackbarType === "success" ? "#33CC33" : "#FFCC33",
+          },
+        }}
+      />
     </div>
   );
 };
