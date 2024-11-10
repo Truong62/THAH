999d8115c9fc27f539511cf8d011f70f193d742b
Truong26 <truongnn@avadagroup.com>
2024-11-11 05:53:43 +0700

    merge minor

--- src/components/404NotFound/_404PageNotFound.jsx
+++ src/components/404NotFound/_404PageNotFound.jsx
@@ -5,7 +5,7 @@ export default function PageNotFound() {
   return (
     <>
       <Header />
-      <div className="h-screen w-screen flex items-center">
+      <div className="h-screen flex items-center">
         <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
           <div className="max-w-md">
             <div className="text-5xl font-dark font-bold">404</div>
