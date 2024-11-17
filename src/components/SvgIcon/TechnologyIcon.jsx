2de73bbf39bc3f5da8e916523748969256bb3950
Huong VH <vuhuyhuong3112004@gmail.com>
2024-11-17 22:10:15 +0700

    responsive

--- src/components/SvgIcon/TechnologyIcon.jsx
+++ src/components/SvgIcon/TechnologyIcon.jsx
@@ -1,8 +1,26 @@
-const TechnologyIcon = ({ width = 48, height = 48, fill = "rgba(255,255,255,1)" }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={width} height={height} fill={fill}>
-      <path d="M6 18H18V6H6V18ZM14 20H10V22H8V20H5C4.44772 20 4 19.5523 4 19V16H2V14H4V10H2V8H4V5C4 4.44772 4.44772 4 5 4H8V2H10V4H14V2H16V4H19C19.5523 4 20 4.44772 20 5V8H22V10H20V14H22V16H20V19C20 19.5523 19.5523 20 19 20H16V22H14V20ZM8 8H16V16H8V8Z" />
-    </svg>
-  );
-  
-  export default TechnologyIcon;
-  
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TechnologyIcon = ({
+  width = 48,
+  height = 48,
+  fill = 'rgba(255,255,255,1)',
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={width}
+    height={height}
+    fill={fill}
+  >
+    <path d="M6 18H18V6H6V18ZM14 20H10V22H8V20H5C4.44772 20 4 19.5523 4 19V16H2V14H4V10H2V8H4V5C4 4.44772 4.44772 4 5 4H8V2H10V4H14V2H16V4H19C19.5523 4 20 4.44772 20 5V8H22V10H20V14H22V16H20V19C20 19.5523 19.5523 20 19 20H16V22H14V20ZM8 8H16V16H8V8Z" />
+  </svg>
+);
+
+TechnologyIcon.propTypes = {
+  width: PropTypes.string,
+  height: PropTypes.string,
+  fill: PropTypes.string,
+};
+
+export default TechnologyIcon;
--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -23,17 +23,16 @@ const Company = () => {
           src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=hai%20b%C3%A0%20tr%C6%B0ng,%20h%C3%A0%20N%E1%BB%99i%20+(My%20Business%20Name)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
         ></iframe>
       </div>
-      
-      <Layout>
 
+      <Layout>
         <div className="mt-8 mb-8">
-          <div className="flex justify-between mb-2">
-            <p className="text-black font-bold text-3xl">
-              Visit us and speak to <br /> our team today{' '}
+          <div className="flex flex-col md:flex-row justify-between mb-2 text-center md:text-left">
+            <p className="text-black font-bold text-3xl mb-2 md:mb-0">
+              Visit us and speak to <br /> our team today
             </p>
             <p className="text-black text-xs">
-              It is a long established fact that . It is a long established fact{' '}
-              <br /> fact that reader will be distracted
+              It is a long established fact that. It is a long established fact
+              <br /> that reader will be distracted
             </p>
           </div>
 
@@ -49,69 +48,70 @@ const Company = () => {
               </h1>
             </div>
           </div>
+
           <div className="text-center mt-8 p-5">
             <h1 className="font-bold text-4xl">
               Hear our story <br /> & learn who we are
             </h1>
             <p className="text-xs mt-4">
-              There are many variations of passages of Lorem Ipsum available,{' '}
+              There are many variations of passages of Lorem Ipsum available,
               <br /> but the majority have suffered alteration in some form, by
-              injected humour,
+              injected humour
             </p>
           </div>
 
-          <div className="flex justify-between gap-4 my-4">
-            <div className="w-1/3 p-20 text-center bg-blue-50 rounded-md ">
-              <h1 className=" text-xl font-bold">10+</h1>
-              <p className=" text-xs ">yrars of service</p>
+          <div className="flex flex-col md:flex-row justify-between gap-4 my-4">
+            <div className="w-full md:w-1/3 p-8 md:p-20 text-center bg-blue-50 rounded-md">
+              <h1 className="text-2xl font-bold">10+</h1>
+              <p className="text-sm md:text-xs">years of service</p>
             </div>
-            <div className="w-1/3 p-20 text-center bg-blue-50 rounded-md ">
-              <h1 className=" text-xl font-bold">20K</h1>
-              <p className=" text-xs ">happy customers</p>
+            <div className="w-full md:w-1/3 p-8 md:p-20 text-center bg-blue-50 rounded-md">
+              <h1 className="text-2xl font-bold">20K</h1>
+              <p className="text-sm md:text-xs">happy customers</p>
             </div>
-            <div className="w-1/3 p-20 text-center bg-blue-50 rounded-md ">
-              <h1 className=" text-xl font-bold">94%</h1>
-              <p className=" text-xs ">customer satisfaction</p>
+            <div className="w-full md:w-1/3 p-8 md:p-20 text-center bg-blue-50 rounded-md">
+              <h1 className="text-2xl font-bold">94%</h1>
+              <p className="text-sm md:text-xs">customer satisfaction</p>
             </div>
           </div>
 
-          <div className="flex justify-between gap-6">
-            <div className="w-1/3 px-16 py-10 rounded-md border border-gray-300 text-center flex flex-col items-center">
+          <div className="flex flex-wrap md:flex-nowrap justify-between gap-6">
+            <div className="w-full md:w-1/3 px-16 py-10 rounded-md border border-gray-300 text-center flex flex-col items-center mb-6 md:mb-0">
               <div className="mb-4">
-                <StrategyIcon/>
+                <StrategyIcon />
               </div>
               <h1 className="font-bold text-xl mb-2">Strategy</h1>
               <p className="text-xs mb-4">
-                There are many variations opassage of Lorem Ipsum available, but
-                the majority have suffered aeratio.{' '}
+                There are many variations of passage of Lorem Ipsum available,
+                but the majority have suffered alteration.
               </p>
               <a className="font-bold text-blue-400" href="#">
                 Learn More
               </a>
             </div>
 
-            <div className="w-1/3 px-16 py-10 rounded-md bg-blue-400 border border-gray-300 text-center flex flex-col items-center">
-              <div className="mb-4 ">
-                <TechnologyIcon/>
+            <div className="w-full md:w-1/3 px-16 py-10 rounded-md bg-blue-400 border border-gray-300 text-center flex flex-col items-center mb-6 md:mb-0">
+              <div className="mb-4">
+                <TechnologyIcon />
               </div>
               <h1 className="font-bold text-xl mb-2">Technology</h1>
               <p className="text-xs mb-4">
-                There are many variations opassage of Lorem Ipsum available, but
-                the majority have suffered aeratio.{' '}
+                There are many variations of passage of Lorem Ipsum available,
+                but the majority have suffered alteration.
               </p>
               <a className="font-bold text-white" href="#">
                 Learn More
               </a>
             </div>
 
-            <div className="w-1/3 px-16 py-10 rounded-md border border-gray-300 text-center flex flex-col items-center">
+            <div className="w-full md:w-1/3 px-16 py-10 rounded-md border border-gray-300 text-center flex flex-col items-center">
               <div className="mb-4">
-                <ConsultingIcon/>
+                <ConsultingIcon />
               </div>
               <h1 className="font-bold text-xl mb-2">Consulting</h1>
               <p className="text-xs mb-4">
-                There are many variations opassage of Lorem Ipsum available, but
-                the majority have suffered aleratio.{' '}
+                There are many variations of passage of Lorem Ipsum available,
+                but the majority have suffered alteration.
               </p>
               <a className="font-bold text-blue-400" href="#">
                 Learn More
