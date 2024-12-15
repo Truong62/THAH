2577f23a05a7ae08ada598dd193b8217a4de75f6
Hyp2k4 <hiephoang1752004@gmail.com>
2024-12-16 03:32:16 +0700

    push skeleton, header, all done

--- BE-demo/index.js
+++ BE-demo/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const cors = require('cors'); // Import cors
+const cors = require('cors');
 const apiRoutes = require('./src/route/api');
 
 dotenv.config();
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,20 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
-import { Collapse, IconButton, Skeleton } from '@mui/material'; // Thêm import Skeleton
+import { Collapse, IconButton } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import axios from 'axios';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { formatCurrency } from '../utils/formatCurrency';
 import introVideo from '../assets/intro.mp4';
 import introVideo2 from '../assets/intro2.mp4';
-
+import useFetchApi from '../hooks/useFetchApi';
+import Header from '../components/Header/Header';
+import SkeletonProduct from '../components/Skeleton/SkeletonProducts';
 const Home = () => {
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
-  const [products, setProducts] = useState([]);
   const [expanded, setExpanded] = useState(false);
   const [currentProduct, setCurrentProduct] = useState({
     id: '',
@@ -26,93 +25,63 @@ const Home = () => {
     tags: [],
     price: 0,
   });
+  const [formattedProducts, setFormattedProducts] = useState([]);
   const swiperRef = useRef(null);
 
+  const {
+    data: products,
+    loading,
+    error,
+  } = useFetchApi('http://localhost:3000/api/product', {
+    limit: 10,
+    page: 1,
+    order: 'desc',
+  });
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await axios.get('http://localhost:3000/api/product', {
-          params: {
-            limit: 10,
-            page: 1,
-            order: 'desc',
-          },
-        });
-
-        const formattedProducts = response.data.data.map((product) => ({
-          id: product.productId,
-          name: product.productName,
-          description: product.productDescription,
-          brand: product.brandName,
-          image: product.imagePath || 'https://placehold.co/300x300',
-          tags: product.tag,
-          price: product.price,
-        }));
-
-        setProducts(formattedProducts);
-        if (formattedProducts.length > 0) {
-          setCurrentProduct(formattedProducts[0]);
-        }
-      } catch (error) {
-        console.error('Fail to get data:', error);
-      } finally {
-        setLoading(false); // Đặt loading thành false sau khi tải xong
-      }
-    };
-
-    fetchProducts();
-  }, []);
+    const newFormattedProducts = products.map((product) => ({
+      id: product.productId,
+      name: product.productName,
+      description: product.productDescription,
+      brand: product.brandName,
+      image: product.imagePath || 'https://placehold.co/300x300',
+      tags: product.tag,
+      price: product.price,
+    }));
+    setFormattedProducts(newFormattedProducts);
+    if (newFormattedProducts.length > 0) {
+      setCurrentProduct(newFormattedProducts[0]);
+    }
+  }, [products]);
 
   const handleNavigation = (direction) => {
     if (swiperRef.current && swiperRef.current.swiper) {
       const swiper = swiperRef.current.swiper;
-      const currentIndex = products.findIndex(
+      const currentIndex = formattedProducts.findIndex(
         (p) => p.id === currentProduct.id
       );
 
-      if (direction === 'next' && currentIndex < products.length - 1) {
-        setCurrentProduct(products[currentIndex + 1]);
-        swiper.slideNext();
-      } else if (direction === 'prev' && currentIndex > 0) {
-        setCurrentProduct(products[currentIndex - 1]);
-        swiper.slidePrev();
+      if (direction === 'next') {
+        if (currentIndex < formattedProducts.length - 1) {
+          setCurrentProduct(formattedProducts[currentIndex + 1]);
+          swiper.slideNext();
+        }
+      } else if (direction === 'prev') {
+        if (currentIndex > 0) {
+          setCurrentProduct(formattedProducts[currentIndex - 1]);
+          swiper.slidePrev();
+        }
       }
     }
   };
 
   return (
     <div className="max-w-[1440px] mx-auto">
-      <nav className="flex flex-col md:flex-row justify-between items-center py-4 px-4 md:px-6">
-        <div className="flex flex-wrap justify-center md:justify-start gap-4 md:gap-8 mb-4 md:mb-0">
-          <a
-            href="#"
-            className="text-sm md:text-base text-gray-600 hover:text-black"
-          >
-            FASHION
-          </a>
-          <a
-            href="/products"
-            className="text-sm md:text-base text-gray-600 hover:text-black"
-          >
-            PRODUCTS
-          </a>
-          <a
-            href="/company"
-            className="text-sm md:text-base text-gray-600 hover:text-black"
-          >
-            COMPANY
-          </a>
-          <a
-            href="#"
-            className="text-sm md:text-base text-gray-600 hover:text-black"
-          >
-            SHOWS
-          </a>
-        </div>
-        <button className="w-full md:w-auto bg-[#5AA1E3] text-white px-4 py-1">
-          CONTACT US
-        </button>
-      </nav>
+      <Header></Header>
 
       <div className="relative w-full h-[200px] md:h-[400px] my-4 md:my-8">
         <video
@@ -136,7 +105,7 @@ const Home = () => {
         <div className="col-span-1 md:col-span-6">
           <div className="aspect-square overflow-hidden group">
             <video
-              className="aspect-square object-cover w-full h-full"
+              className="w-full h-full object-cover"
               autoPlay
               loop
               muted
@@ -208,33 +177,96 @@ const Home = () => {
             loop={false}
             className="product-slider"
             onSlideChange={(swiper) => {
-              setCurrentProduct(products[swiper.activeIndex]);
+              setCurrentProduct(formattedProducts[swiper.activeIndex]);
             }}
           >
             {loading
               ? Array.from({ length: 10 }).map((_, index) => (
-                  <SwiperSlide key={index} className="!w-[300px] md:!w-[350px]">
-                    <Skeleton variant="rectangular" width="100%" height={350} />
+                  <SwiperSlide
+                    key={`skeleton-${index}`}
+                    className="!w-[300px] md:!w-[350px]"
+                  >
+                    <div className="bg-gray-50 p-4 h-full">
+                      {/* Ảnh skeleton: Vuông */}
+                      <div className="mb-4">
+                        <SkeletonProduct shape="square" size="10rem" />
+                      </div>
+
+                      {/* Tên sản phẩm skeleton: Hình chữ nhật */}
+                      <div className="mb-2">
+                        <SkeletonProduct width="80%" height="1rem" />
+                      </div>
+
+                      {/* Giá sản phẩm skeleton: Hình chữ nhật nhỏ */}
+                      <div className="mb-2">
+                        <SkeletonProduct width="50%" height="1rem" />
+                      </div>
+
+                      {/* Tags skeleton: Nhiều hình chữ nhật nhỏ */}
+                      <div className="flex flex-wrap gap-2">
+                        {Array.from({ length: 3 }).map((_, tagIndex) => (
+                          <SkeletonProduct
+                            key={`tag-skeleton-${tagIndex}`}
+                            width="4rem"
+                            height="0.8rem"
+                          />
+                        ))}
+                      </div>
+                    </div>
                   </SwiperSlide>
                 ))
-              : products.map((product) => (
+              : formattedProducts.map((product) => (
                   <SwiperSlide
                     key={product.id}
                     className="!w-[300px] md:!w-[350px]"
+                    onClick={() =>
+                      setCurrentProduct({
+                        id: product.id,
+                        name: product.name,
+                        description: product.description,
+                        brand: product.brand,
+                        image: product.image,
+                        tags: product.tags,
+                        price: product.price,
+                      })
+                    }
                   >
                     {!product.image ||
                     product.image.trim() === '' ||
                     !product.name ||
                     !product.price ? (
-                      <Skeleton
-                        variant="rectangular"
-                        width="100%"
-                        height={350}
-                      />
+                      <div className="bg-gray-50 p-4 h-full">
+                        {/* Ảnh skeleton */}
+                        <div className="mb-4">
+                          <SkeletonProduct shape="square" size="10rem" />
+                        </div>
+
+                        {/* Tên sản phẩm skeleton */}
+                        <div className="mb-2">
+                          <SkeletonProduct width="80%" height="1rem" />
+                        </div>
+
+                        {/* Giá sản phẩm skeleton */}
+                        <div className="mb-2">
+                          <SkeletonProduct width="50%" height="1rem" />
+                        </div>
+
+                        {/* Tags skeleton */}
+                        <div className="flex flex-wrap gap-2">
+                          {Array.from({ length: 3 }).map((_, tagIndex) => (
+                            <SkeletonProduct
+                              key={`tag-skeleton-${tagIndex}`}
+                              width="4rem"
+                              height="0.8rem"
+                            />
+                          ))}
+                        </div>
+                      </div>
                     ) : (
                       <div
-                        className={`bg-gray-50 cursor-pointer group relative h-full ${currentProduct.id === product.id ? 'bg-gray-200' : ''}`}
-                        onClick={() => setCurrentProduct(product)}
+                        className={`bg-gray-50 cursor-pointer group relative h-full ${
+                          currentProduct.id === product.id ? 'bg-gray-200' : ''
+                        }`}
                       >
                         <div className="relative w-full h-[350px]">
                           <img
@@ -283,7 +315,7 @@ const Home = () => {
 
           <div className="text-center mt-8">
             <button className="w-full md:w-auto bg-[#5AA1E3] text-white px-8 md:px-16 py-2">
-              <a href="/products">SEE MORE PRODUCTS</a>
+              <Link to="/products">SEE MORE PRODUCTS</Link>{' '}
             </button>
           </div>
         </div>
